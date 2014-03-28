baeed22857ae17a9adb89934f025f59f33010856
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-28 13:24:30 +0100

    repo created

---
+++ src/main/java/de/metalcon/testing/MuidFactory.java
@@ -0,0 +1,37 @@
+package de.metalcon.testing;
+
+import de.metalcon.domain.Muid;
+import de.metalcon.domain.MuidType;
+import de.metalcon.exceptions.ServiceOverloadedException;
+
+/**
+ * MUID factory
+ * 
+ * @author sebschlicht
+ * 
+ */
+public class MuidFactory {
+
+    /**
+     * generate a MUID<br>
+     * waits until the MUID is created, if there were too many MUIDs created for
+     * the type requested
+     * 
+     * @param muidType
+     *            type of the MUID requested
+     * @return new MUID
+     */
+    public static Muid generateMuid(MuidType muidType) {
+        try {
+            return Muid.create(muidType);
+        } catch (ServiceOverloadedException e) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e1) {
+                // thread interrupted
+            }
+            return generateMuid(muidType);
+        }
+    }
+
+}

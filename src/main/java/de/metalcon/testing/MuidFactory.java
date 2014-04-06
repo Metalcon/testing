09044cd8215a2f44d4fa15bb0fd7cc76a354d6c1
Jonas Kunze <jonas@metalcon.de>
2014-04-06 21:09:04 +0200

    Switched to newset muid version

--- src/main/java/de/metalcon/testing/MuidFactory.java
+++ src/main/java/de/metalcon/testing/MuidFactory.java
@@ -1,7 +1,7 @@
 package de.metalcon.testing;
 
 import de.metalcon.domain.Muid;
-import de.metalcon.domain.MuidType;
+import de.metalcon.domain.UidType;
 import de.metalcon.exceptions.ServiceOverloadedException;
 
 /**
@@ -12,26 +12,26 @@ import de.metalcon.exceptions.ServiceOverloadedException;
  */
 public class MuidFactory {
 
-    /**
-     * generate a MUID<br>
-     * waits until the MUID is created, if there were too many MUIDs created for
-     * the type requested
-     * 
-     * @param muidType
-     *            type of the MUID requested
-     * @return new MUID
-     */
-    public static Muid generateMuid(MuidType muidType) {
-        try {
-            return Muid.create(muidType);
-        } catch (ServiceOverloadedException e) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e1) {
-                // thread interrupted
-            }
-            return generateMuid(muidType);
-        }
-    }
+	/**
+	 * generate a MUID<br>
+	 * waits until the MUID is created, if there were too many MUIDs created for
+	 * the type requested
+	 * 
+	 * @param muidType
+	 *            type of the MUID requested
+	 * @return new MUID
+	 */
+	public static Muid generateMuid(final UidType muidType) {
+		try {
+			return Muid.create(muidType);
+		} catch (ServiceOverloadedException e) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e1) {
+				// thread interrupted
+			}
+			return generateMuid(muidType);
+		}
+	}
 
 }
